ef74cf20a8802cb4071108bf771a9c7c20301afb
Shivan Singh <shivan.0972@gmail.com>
2022-07-12 04:04:28 +0530

    send res data

---
+++ assets/js/res.js
@@ -0,0 +1,28 @@
+$( "#req_sum" ).click(function() {
+	var value1=document.getElementById('name').value
+	var value2=document.getElementById("no").value
+	var value3=document.getElementById("email").value
+	var value4=document.getElementById("med").value
+	var value5=document.getElementById("address").value
+	const api = axios.create({baseURL: 'http://localhost:3001'})
+	var payload = {
+	    'name': value1,
+	    'email': value2,
+	    'phoneNumer': value3,
+	    'medicine': value4,
+	    'address': value5
+	}
+	// api.post('/api', payload)
+	// .then(res => {
+	//      console.log(res)
+	// })
+	// .catch(error => {
+	//      console.log(error)
+	// })
+	axios.post('http://localhost:3001/api', payload, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+    )
+});
